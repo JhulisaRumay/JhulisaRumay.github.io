9e21e088d476aef47f796522152747db98cefe28
Jhulisa Rumay Muñoz <jrumaym17_1@unc.edu.pe>
2021-09-04 21:31:34 -0500

    boton Colorear

--- script/lab05.js
+++ script/lab05.js
@@ -1,16 +1,45 @@
-function extraer(){
-    let nombre = document.getElementById('apellidoNombre').ariaValueMax;
-    let partes = nombre.split("");
+let flag=1;
 
-    document.getElementById('apellidoPaterno').ariaValueMax=partes[0];
-    document.getElementById('apellidoMaterno').ariaValueMax=partes[1];
-    for(let i=0; i<partes.length; i++){
-        if(i>= 2){
-            document.getElementById("nombres").ariaValueMax += partes[i] + " ";
-        }
+function colorear(){
+    if(flag){
+        document.getElementById('apeNom').style.background= "red";
+        document.getElementById('fecNac').style.background= "red";
+        document.getElementById('apePat').style.background= "red";
+        document.getElementById('apeMat').style.background= "red";
+        document.getElementById('nomb').style.background= "red";
+        document.getElementById('longApe').style.background= "blue";
+        document.getElementById('ed').style.background= "blue";
+        document.getElementById('mesLet').style.background= "blue";
+        document.getElementById('cambiarColor').style.background= "blue";
+       
+        flag=0;
+    }else{
+        document.getElementById('apeNom').style.background= "blue";
+        document.getElementById('fecNac').style.background= "blue";
+        document.getElementById('apePat').style.background= "blue";
+        document.getElementById('apeMat').style.background= "blue";
+        document.getElementById('nomb').style.background= "blue";
+        document.getElementById('longApe').style.background= "red";
+        document.getElementById('ed').style.background= "red";
+        document.getElementById('mesLet').style.background= "red";
+        document.getElementById('cambiarColor').style.background= "red";
+       
+        flag=1;
     }
+}
+function extraer(){
+    let nombre = document.getElementById('apellidoNombre');
+    let partes = nombre.split(" ");
+
+    document.getElementById('apellidoPaterno')=partes[0];
+    document.getElementById('apellidoMaterno')=partes[1];
+   for(let i=0; i<partes.length; i++){
+     if(i>= 2){
+      document.getElementById("nombres") += partes[i] + " ";
+     }
+   }
     document.getElementById('apellidoPaterno').style.color = "black";
     document.getElementById('apellidoMaterno').style.color = "black";
-    document.getElementById('nombres').style.color = "black";
+  document.getElementById('nombres').style.color = "black";
 
 }
